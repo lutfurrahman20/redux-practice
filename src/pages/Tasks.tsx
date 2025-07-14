133990d70c5647cdfc3abd2479314eeec6179222
lutfur rahman siddiquee <121648644+lutfurrahman20@users.noreply.github.com>
2025-07-14 07:53:23 +0600

    updated task tab

--- src/pages/Tasks.tsx
+++ src/pages/Tasks.tsx
@@ -1,23 +1,52 @@
 import { AddTaskModal } from "@/components/module/tasks/AddTaskModal";
 import TaskCard from "@/components/module/tasks/TaskCard";
-import { selectTasks } from "@/redux/features/task/taskSlice";
-import { useAppSelector } from "@/redux/hook";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { selectTasks, updateFilter } from "@/redux/features/task/taskSlice";
+import { useAppDispatch, useAppSelector } from "@/redux/hook";
 
 const Tasks = () => {
   const tasks = useAppSelector(selectTasks);
 
-  console.log(tasks);
+  const dispatch = useAppDispatch();
 
   return (
     <div className="mx-auto max-w-7xl px-5 mt-20">
-      <div className="flex justify-between items-center">
-        <h1>Tasks</h1>
-        <AddTaskModal/>
+      <div className="flex justify-end items-center gap-5">
+        <h1 className="mr-auto">Tasks</h1>
+        <Tabs defaultValue="all">
+          <TabsList className="grid w-full grid-cols-4">
+            <TabsTrigger
+              onClick={() => dispatch(updateFilter("all"))}
+              value="all"
+            >
+              All
+            </TabsTrigger>
+            <TabsTrigger
+              onClick={() => dispatch(updateFilter("high"))}
+              value="high"
+            >
+              High
+            </TabsTrigger>
+            <TabsTrigger
+              onClick={() => dispatch(updateFilter("medium"))}
+              value="medium"
+            >
+              Medium
+            </TabsTrigger>
+            <TabsTrigger
+              onClick={() => dispatch(updateFilter("low"))}
+              value="low"
+            >
+              Low
+            </TabsTrigger>
+          </TabsList>
+        </Tabs>
+        <AddTaskModal />
       </div>
       <div className="space-y-5 mt-5">
-        {
-            tasks.map(task => <TaskCard task={task} key={task.id}/>)
-        }
+        {tasks.map((task) => (
+          <TaskCard task={task} key={task.id} />
+        ))}
       </div>
     </div>
   );
--- src/redux/features/task/taskSlice.ts
+++ src/redux/features/task/taskSlice.ts
@@ -41,31 +41,45 @@ const taskSlice = createSlice({
           : task
       );
     },
-    updateTask: (
-      state,
-      action: PayloadAction<{ id: string; newTask: string }>
-    ) => {
-      const { id, newTask } = action.payload;
-      state.tasks.forEach((task) => {
-        if (task.id === id) {
-          task.title = newTask;
-        }
-      });
-    },
+    // updateTask: (
+    //   state,
+    //   action: PayloadAction<{ id: string; newTask: string }>
+    // ) => {
+    //   const { id, newTask } = action.payload;
+    //   state.tasks.forEach((task) => {
+    //     if (task.id === id) {
+    //       task.title = newTask;
+    //     }
+    //   });
+    // },
 
     deleteTask: (state, action: PayloadAction<string>) => {
       state.tasks = state.tasks.filter((task) => task.id !== action.payload);
+    },
+    updateFilter: (
+      state,
+      action: PayloadAction<"all" | "low" | "medium" | "high">
+    ) => {
+      state.filter = action.payload;
     },
   },
 });
 
 export const selectTasks = (state: RootState) => {
-  return state.todo.tasks;
+  const filter = state.todo.filter;
+
+  if (filter === "low") {
+    return state.todo.tasks.filter((task) => task.priority === "low");
+  } else if (filter === "medium") {
+    return state.todo.tasks.filter((task) => task.priority === "medium");
+  } else if (filter === "high") {
+    return state.todo.tasks.filter((task) => task.priority === "high");
+  } else return state.todo.tasks;
 };
 export const selectFilter = (state: RootState) => {
   return state.todo.filter;
 };
 
-export const { addTask, toggleCompleteState, deleteTask, updateTask } =
+export const { addTask, toggleCompleteState, deleteTask, updateFilter } =
   taskSlice.actions;
 export default taskSlice.reducer;
